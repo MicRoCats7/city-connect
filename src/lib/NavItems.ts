544d5e57cf3261026897ba840ac37c2d4fcb9be0
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2024-11-11 22:22:19 +0700

    slicing navbar

---
+++ src/lib/NavItems.ts
@@ -0,0 +1,22 @@
+export const NavItems = [
+    {
+        title: "Beranda",
+        url: "/",
+    },
+    {
+        title: "Artikel",
+        url: "/services",
+    },
+    {
+        title: "Kenali Smart City",
+        url: "/products",
+    },
+    {
+        title: "Galeri",
+        url: "/products",
+    },
+    {
+        title: "Peta Smart City",
+        url: "/products",
+    }
+]

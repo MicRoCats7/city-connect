b1c23df53899a61d9faf6a43c0dda5be0d0ad2c5
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2024-11-12 17:59:24 +0700

    finish ui news

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -10,23 +10,35 @@ function News() {
                     <Separator className="border-2 rounded-full w-[94px]" />
                 </div>
                 <div className="mt-16">
-                    <h1 className="special-text font-general-sans-semibold text-3xl mb-5">Must Read</h1>
                     <div className="grid grid-cols-4 grid-rows-2 gap-2.5">
-                        {berita.map((news) => {
-                            return (
-                                <div key={news.id} className={`relative col-span-2 row-span-2`}>
-                                    <div className="bg-gradient-to-b from-transparent to-black absolute w-full h-full rounded-[20px]"></div>
-                                    <img
-                                        className={`mb-5 object-cover w-full h-full rounded-[20px]`}
-                                        // src={news.thumbnail}
-                                        alt={news.title}
-                                    />
-                                    <div className="absolute bottom-0 p-5">
-                                        <p className="mb-5 text-[22px] font-general-sans-semibold">{news.title}</p>
+                        {berita.map((news, index) => {
+                            if (index !== 2) {
+                                return (
+                                    <div key={news.id} className="relative col-span-1 flex flex-col justify-between cursor-pointer bg-transparent">
+                                        <img
+                                            className="mb-5 w-[270px] h-[180px] rounded-[20px] hover:scale-105 transition transform"
+                                            src={news.image}
+                                            alt={news.title}
+                                        />
+                                        <p className="mb-5 text-[22px] leading-6 font-general-sans-semibold">{news.title}</p>
                                         <p className="font-general-sans-medium text-base opacity-60">{news.date}</p>
                                     </div>
-                                </div>
-                            )
+                                )
+                            } else {
+                                return (
+                                    <div key={news.id} className="relative col-span-2 row-span-2 flex flex-col justify-between cursor-pointer hover:scale-105 transition transform">
+                                        <img
+                                            className="mb-5 object-cover w-full h-full rounded-[20px]"
+                                            src={news.image}
+                                            alt={news.title}
+                                        />
+                                        <div className="absolute -bottom-4 p-5 bg-gradient-to-t from-black/50 to-transparent">
+                                            <p className="mb-5 text-[22px] font-general-sans-semibold">{news.title}</p>
+                                            <p className="font-general-sans-medium text-base opacity-60">{news.date}</p>
+                                        </div>
+                                    </div>
+                                )
+                            }
                         })}
                     </div>
                 </div>
